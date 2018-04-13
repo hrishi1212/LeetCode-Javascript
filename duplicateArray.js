b4d5389f325bbfaf8a6d5f2ecb25c5cf6f3e95c3
Hrishikesh Kale <hrishikeshkale1212@gmail.com>
2018-04-13 13:30:25 +0530

    Create duplicateArray.js

---
+++ duplicateArray.js
@@ -0,0 +1,14 @@
+var removeDuplicates = function(nums) {
+   if (nums.length == 0) return 0;
+    
+    var i = 0;
+    
+    for (var j = 1; j < nums.length; j++) {
+        if (nums[j] != nums[i]) {
+            i++;
+            nums[i] = nums[j];
+        }
+    }
+    return i + 1;
+
+};
